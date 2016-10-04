dc5a5d136f48d423d1452401765d8da7c911b419
rthome <tyler_thome@yahoo.com>
2016-10-04 00:09:19 -0700

    working

--- main.cpp
+++ main.cpp
@@ -5,27 +5,29 @@
 #include <iostream>
 #include "server.h"
 #include "client.h"
-#include "Peer.h"
 #include <arpa/inet.h>
+#include <ctype.h>
 
- void *listen_routine(void *port){
+#define NUM_THREADS     5
+
+void *listen_routine(void *port){
     std::cout << "\n In listen_routine\nGoing to listen on " << (char*)port;
     server_main((char*)port);
     pthread_exit(NULL);
- }
+}
 
- void *send_routine(void *port){
+void *send_routine(void *port){
     char input[100];
-    std::cout << "\n\nEnter a message: ";
+    std::cout << "\n\nEnter a message!!!!!: ";
     //std::cin.ignore();
     std::cin.getline(input, sizeof(input));
-    client_main("localhost", (char *)port, input); 
-    std::cout << "\n\nMessage sent: " << "\"" << input << "\"";
-
-
+    client_main("localhost", (char *)port, input);
+    std::cout << "\n\nMessage sent????: " << "\"" << input << "\"";
+    
+    
     pthread_exit(NULL);
-  
- }
+    
+}
 
 
 void display_help(){
@@ -47,59 +49,114 @@ void display_help(){
 }
 
 
- int main (int argc, char *argv[]){
-     
-     std::cout << "\nHi, type \"help\" for assistance:\n\t1) Listen on " << argv[1] << "\n\t2) Send on " << argv[2] << "\n\nChoice: ";
-     std::string choice;
-     std::cin >> choice;
-     
+int main (int argc, char *argv[]){
+    
+   /* std::cout << "\nHi, type \"help\" for assistance:\n\t1) SERVER - Listen on " << argv[1]
+              << "\n\tsend <message>: CLIENT - Send on " << argv[2] << "\n\n>> ";
+    
+    char input[100];
+    std::string choice;
+    std::cin >> choice;
+    bool exit = true;
+    
+*/
+    /*do{
+        
+        if(choice == "1"){
+            server_main(argv[1]);
+            
+            std::cout << ">> ";
+            std::cin >> choice;
+        }
 
-     if(choice == "1"){
-         server_main(argv[1]);
-     }
-  
-     else if (choice == "2") {
-         while(1){
-             char input[100];
-             std::cout << "\n\nEnter a message: ";
-             std::cin.getline(input, sizeof(input));
-             client_main("localhost", argv[2], input);
-             std::cout << "\n\nMessage sent: " << "\"" << input << "\"";
+        else if(choice == "help" || choice == "HELP"){
+            display_help();
+        }
+        
+        else if (choice == "myip" || choice == "MYIP"){
+            std::cout << "\nThe IP address is: \n\n";
+        }
+        
+        else if (choice == "myport" || choice == "MYPORT"){
+            std::cout << "\nThe port no. is: " << argv[1] << "\n\n";
+        }
+        
+        
+        else if (choice == "connect" || choice == "CONNECT"){
+            std::cin.ignore();
+            std::cin.getline(input, sizeof(input));
+            
+            std::cout << "\n\nConnecting with " << input << " ......\n\n";
+        }
+        
+        else if (choice == "list" || choice == "LIST"){
+            std::cout << "\n\nLISTING CONNECTIONS:\n\n";
+        }
 
-         }
-    }
-     
-    else if(choice == "help" || choice == "HELP"){
-         display_help();
-          
-    }
-  
-    //exit(0);
+        else if (choice == "terminate" || choice == "TERMINATE"){
+            std::cin.ignore();
+            std::cin.getline(input, sizeof(input));
+            
+            std::cout << "\n\nTerminating with " << input << " ......\n\n";
+        }
 
+        
+        else if (choice == "send") {
+            std::cin.ignore();
+            //std::cout << "\n\nEnter a message: ";
+            
+            std::cin.getline(input, sizeof(input));
+            client_main("localhost", argv[2], input);
+            std::cout << "Message that was sent: " << "\"" << input << "\"\n\n";
+        }
+        
+        if (choice == "exit" || choice == "EXIT"){
+            exit = true;
+            break;
+        }
+        
+        
+        std::cout << ">> ";
+        std::cin >> choice;
+        if (choice != "send" || choice != "connect" || choice != "terminate"){
+           // std::cout << "CHOICE: " << choice;
+        }
+        
+    } while (choice != "exit");
+    
+    std::cout << "\n+--------------------------------------------------+\n"
+                << "|                 SIGNING OUT....                  |\n"
+                << "+--------------------------------------------------+\n";
 
+    
+
+    */
+
+    
     pthread_t listener_thread;
     pthread_t sender_thread;
-
-    std::cout << "\nGoing to listen on " << argv[1] 
-              << " and broadcast on "    << argv[2]
-              << ",\n\targ[0] was \""      << argv[0] << "\"\n";
-
+    
+    std::cout << "\nGoing to listen on " << argv[1]
+    << " and broadcast on "    << argv[2]
+    << ",\n\targ[0] was \""      << argv[0] << "\"\n";
+    
     int listener_thread_error =
-        pthread_create(&listener_thread, NULL, listen_routine, (void *)argv[1]);
-     
-     
+    pthread_create(&listener_thread, NULL, listen_routine, (void *)argv[1]);
+    
+    
     if(listener_thread_error){
-      std::cout << "\nError creating listener: code " << listener_thread_error;
-     }
-
+        std::cout << "\nError creating listener: code " << listener_thread_error;
+    }
+    
     int sender_thread_error =
-        pthread_create(&sender_thread, NULL, send_routine, (void *)argv[2]);
-
-
+    pthread_create(&sender_thread, NULL, send_routine, (void *)argv[2]);
+    
+    
     if(sender_thread_error){
         std::cout << "\nError creating sender: code " << sender_thread_error;
     }
-     
-    /* Last thing that main() should do */
+    
+    // Last thing that main() should do
     pthread_exit(NULL);
- }
+     
+}
